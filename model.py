82e04f8c14563cdbb572a1b704d620a1d54c5eea
Arka Saha <40143090+ArkaJU@users.noreply.github.com>
2018-10-23 08:56:00 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,211 @@
+import os
+import time
+from glob import glob
+import tensorflow as tf
+import numpy as np
+from collections import namedtuple
+
+from module import *
+from utils import *
+
+def load_weights(saver, sess, model_dir):
+
+    ckpt = tf.train.get_checkpoint_state(model_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+        ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+        saver.restore(sess, os.path.join(model_dir, ckpt_name))
+        return True
+    else:
+        return False
+
+
+def Save(saver, sess, checkpoint_dir, step):
+    dir = os.path.join(checkpoint_dir, "model")
+    saver.save(sess, dir)
+    
+    
+class cyclegan(object):
+    def __init__(self, sess):
+        self.sess = sess
+        self.batch_size = 1
+        self.image_size = 256
+        self.load_size = 286
+        self.fine_size = 256
+        self.train_size = 1e8
+        self.input_c_dim = 3        # of input image channels            
+        self.output_c_dim = 3       # of output image channels
+        self.L1_lambda = 10.0
+        self.dataset_dir = 'iphone2dslr_flower'
+        self.ndf = 64
+        self.ngf = 64
+        
+        self.phase = 'train'
+
+        self.discriminator = discriminator
+        self.generator = generator_resnet
+        self.criterionGAN = mae_criterion
+        
+        OPTIONS = namedtuple('OPTIONS', 'batch_size image_size \
+                              gf_dim df_dim output_c_dim is_training')
+        self.options = OPTIONS._make((self.batch_size, self.fine_size,
+                                      self.ngf, self.ndf, self.output_c_dim, self.phase == 'train'))
+                                                                           
+
+        self._build_model()
+        self.saver = tf.train.Saver()
+        self.pool = ImagePool(50)
+
+    def _build_model(self):
+        self.real_data = tf.placeholder(tf.float32,
+                                        [None, self.image_size, self.image_size,
+                                         self.input_c_dim + self.output_c_dim],
+                                        name='real_A_and_B_images')
+
+        self.real_A = self.real_data[:, :, :, :self.input_c_dim]
+        self.real_B = self.real_data[:, :, :, self.input_c_dim:self.input_c_dim + self.output_c_dim]
+
+        self.fake_B = self.generator(self.real_A, self.options, False, name="generatorA2B")
+        self.fake_A_ = self.generator(self.fake_B, self.options, False, name="generatorB2A")
+        self.fake_A = self.generator(self.real_B, self.options, True, name="generatorB2A")
+        self.fake_B_ = self.generator(self.fake_A, self.options, True, name="generatorA2B")
+
+        self.DB_fake = self.discriminator(self.fake_B, self.options, reuse=False, name="discriminatorB")
+        self.DA_fake = self.discriminator(self.fake_A, self.options, reuse=False, name="discriminatorA")
+        
+        self.g_loss_a2b = self.criterionGAN(self.DB_fake, tf.ones_like(self.DB_fake)) \
+            + self.L1_lambda * abs_criterion(self.real_A, self.fake_A_) \
+            + self.L1_lambda * abs_criterion(self.real_B, self.fake_B_)
+        
+        self.g_loss_b2a = self.criterionGAN(self.DA_fake, tf.ones_like(self.DA_fake)) \
+            + self.L1_lambda * abs_criterion(self.real_A, self.fake_A_) \
+            + self.L1_lambda * abs_criterion(self.real_B, self.fake_B_)
+        
+        self.g_loss = self.criterionGAN(self.DA_fake, tf.ones_like(self.DA_fake)) \
+            + self.criterionGAN(self.DB_fake, tf.ones_like(self.DB_fake)) \
+            + self.L1_lambda * abs_criterion(self.real_A, self.fake_A_) \
+            + self.L1_lambda * abs_criterion(self.real_B, self.fake_B_)
+
+        
+        self.fake_A_sample = tf.placeholder(tf.float32,
+                                            [None, self.image_size, self.image_size,
+                                             self.input_c_dim], name='fake_A_sample')
+        self.fake_B_sample = tf.placeholder(tf.float32,
+                                            [None, self.image_size, self.image_size,
+                                             self.output_c_dim], name='fake_B_sample')
+        
+        self.DB_real = self.discriminator(self.real_B, self.options, reuse=True, name="discriminatorB")
+        self.DA_real = self.discriminator(self.real_A, self.options, reuse=True, name="discriminatorA")
+        self.DB_fake_sample = self.discriminator(self.fake_B_sample, self.options, reuse=True, name="discriminatorB")
+        self.DA_fake_sample = self.discriminator(self.fake_A_sample, self.options, reuse=True, name="discriminatorA")
+
+        
+        self.db_loss_real = self.criterionGAN(self.DB_real, tf.ones_like(self.DB_real))
+        self.db_loss_fake = self.criterionGAN(self.DB_fake_sample, tf.zeros_like(self.DB_fake_sample))
+        self.db_loss = (self.db_loss_real + self.db_loss_fake) / 2
+        
+        self.da_loss_real = self.criterionGAN(self.DA_real, tf.ones_like(self.DA_real))
+        self.da_loss_fake = self.criterionGAN(self.DA_fake_sample, tf.zeros_like(self.DA_fake_sample))
+        self.da_loss = (self.da_loss_real + self.da_loss_fake) / 2
+        
+        self.d_loss = self.da_loss + self.db_loss
+
+        self.g_loss_a2b_sum = tf.summary.scalar("g_loss_a2b", self.g_loss_a2b)
+        self.g_loss_b2a_sum = tf.summary.scalar("g_loss_b2a", self.g_loss_b2a)
+        
+        self.g_loss_sum = tf.summary.scalar("g_loss", self.g_loss)
+        self.g_sum = tf.summary.merge([self.g_loss_a2b_sum, self.g_loss_b2a_sum, self.g_loss_sum])
+        
+        
+        self.db_loss_sum = tf.summary.scalar("db_loss", self.db_loss)
+        self.da_loss_sum = tf.summary.scalar("da_loss", self.da_loss)
+        self.d_loss_sum = tf.summary.scalar("d_loss", self.d_loss)
+        
+        self.db_loss_real_sum = tf.summary.scalar("db_loss_real", self.db_loss_real)
+        self.db_loss_fake_sum = tf.summary.scalar("db_loss_fake", self.db_loss_fake)
+        self.da_loss_real_sum = tf.summary.scalar("da_loss_real", self.da_loss_real)
+        self.da_loss_fake_sum = tf.summary.scalar("da_loss_fake", self.da_loss_fake)
+        self.d_sum = tf.summary.merge([self.da_loss_sum, self.da_loss_real_sum, self.da_loss_fake_sum,
+                                       self.db_loss_sum, self.db_loss_real_sum, self.db_loss_fake_sum,
+                                       self.d_loss_sum])
+
+       
+        t_vars = tf.trainable_variables()
+        self.d_vars = [var for var in t_vars if 'discriminator' in var.name]
+        self.g_vars = [var for var in t_vars if 'generator' in var.name]
+        for var in t_vars: print(var.name)
+
+    def train(self):
+        """Train cyclegan"""
+        
+        self.lr = tf.placeholder(tf.float32, None, name='learning_rate')
+        self.d_optim = tf.train.AdamOptimizer(self.lr, beta1=0.5) \
+            .minimize(self.d_loss, var_list=self.d_vars)
+        self.g_optim = tf.train.AdamOptimizer(self.lr, beta1=0.5) \
+            .minimize(self.g_loss, var_list=self.g_vars)
+            
+        sum1 = tf.summary.image("realA", self.real_A, max_outputs=1)    
+        sum2 = tf.summary.image("fakeB", self.fake_B, max_outputs=1)
+        sum3 = tf.summary.image("fakeA_", self.fake_A_, max_outputs=1)
+        sum4 = tf.summary.image("realB", self.real_B, max_outputs=1)    
+        sum5 = tf.summary.image("fakeA", self.fake_A, max_outputs=1)
+        sum6 = tf.summary.image("fakeB_", self.fake_B_, max_outputs=1)
+        self.image = tf.summary.merge([sum1, sum2, sum3, sum4, sum5, sum6])
+        
+        init_op = tf.global_variables_initializer()
+        self.sess.run(init_op)
+        saver = tf.train.Saver()
+        load_weights(self.saver, self.sess, '/content/drive/My Drive/weights/')
+        self.writer = tf.summary.FileWriter("./logs", self.sess.graph)
+
+        counter = 1
+        start_time = time.time()
+        
+
+        lr = 0.0002
+        self.epochs = 100
+        for epoch in range(self.epochs):
+            dataA = glob('./datasets/{}/*.*'.format(self.dataset_dir + '/trainA'))
+            dataB = glob('./datasets/{}/*.*'.format(self.dataset_dir + '/trainB'))
+            np.random.shuffle(dataA)
+            np.random.shuffle(dataB)
+            batch_idxs = min(min(len(dataA), len(dataB)), self.train_size) // self.batch_size
+
+            
+            
+
+            for idx in range(0, batch_idxs):
+                batch_files = list(zip(dataA[idx * self.batch_size:(idx + 1) * self.batch_size],
+                                       dataB[idx * self.batch_size:(idx + 1) * self.batch_size]))
+                batch_images = [load_train_data(batch_file, self.load_size, self.fine_size) for batch_file in batch_files]
+                batch_images = np.array(batch_images).astype(np.float32)
+
+                
+                
+                # Update G network and record fake outputs
+                fake_A, fake_B, _, summary_str, summary_str2 = self.sess.run(
+                    [self.fake_A, self.fake_B, self.g_optim, self.g_sum, self.image],
+                    feed_dict={self.real_data: batch_images, self.lr: lr})
+                self.writer.add_summary(summary_str, counter)
+                self.writer.add_summary(summary_str2, counter)
+                [fake_A, fake_B] = self.pool([fake_A, fake_B])
+
+                # Update D network
+                _, summary_str = self.sess.run(
+                    [self.d_optim, self.d_sum],
+                    feed_dict={self.real_data: batch_images,
+                               self.fake_A_sample: fake_A,
+                               self.fake_B_sample: fake_B,
+                               self.lr: lr})
+                self.writer.add_summary(summary_str, counter)
+
+                counter += 1
+                print(("Epoch: [%2d] [%4d/%4d] time: %4.4f" % (
+                    epoch, idx, batch_idxs, time.time() - start_time)))
+                
+                if idx % 1000 == 0:    
+                    Save(self.saver, self.sess, '/content/drive/My Drive/weights/', epoch)
+
+            
+            
+
+    
